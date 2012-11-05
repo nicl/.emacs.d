dc1acf2f0eeb3e06b24126f84a2092dc1827c33e
Nicolas Long <nicolaslong@gmail.com>
2012-11-05 15:06:52 +0000

    added gitignore and basic init.el

---
+++ README.md
@@ -0,0 +1,8 @@
+Custom emacs setup
+==================
+
+This is my custom emacs setup. I rely on the [emacs starter kit](https://github.com/technomancy/emacs-starter-kit) for my defaults.
+
+The rest is mainly to ensure some useful packages are installed.
+
+I mostly use Emacs for writing Clojure code at the moment. For PHP and JS I tend to default to Sublime Text 2.
---
+++ init.el
@@ -0,0 +1,36 @@
+;;; marmalade + packages
+;;; ------------------------------------------------------------------
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit
+                      starter-kit-lisp
+                      starter-kit-bindings
+                      starter-kit-eshell
+                      clojure-mode
+                      clojure-test-mode
+                      fill-column-indicator
+                      auto-complete
+                      smooth-scrolling)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;;; fill-column-indicator
+;;; ------------------------------------------------------------------
+(add-hook 'after-change-major-mode-hook 'fci-mode)
+
+;;; save session
+;;; ------------------------------------------------------------------
+(desktop-save-mode 1)
+
+;;; prevent backups (git is my VCS)
+;;; ------------------------------------------------------------------
+(setq make-backup-files nil)
