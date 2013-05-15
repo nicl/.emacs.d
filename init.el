0169513eb83bc2953df9c3a44e21768cc8ed8182
Nicolas Long <nicolas.long@guardian.co.uk>
2013-05-15 14:54:23 +0100

    added ensime

--- init.el
+++ init.el
@@ -43,3 +43,11 @@
 ;;; ------------------------------------------------------------------
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
+;; load the ensime lisp code...
+(add-to-list 'load-path "~/.emacs.d/ensime/elisp/")
+(require 'ensime)
+
+;; This step causes the ensime-mode to be started whenever
+;; scala-mode is started for a buffer. You may have to customize this step
+;; if you're not using the standard scala mode.
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
